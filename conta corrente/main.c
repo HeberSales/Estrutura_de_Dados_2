33ea810ee36327bd861810c2840cf8409713e654
HeberSales <heberssales@gmail.com>
2024-03-08 21:19:59 -0300

    Exercicio 1 - que preciso fazer

---
+++ conta corrente/main.c
@@ -0,0 +1,13 @@
+/*
+->Modifique a implementação para lidar com registros de 
+conta-corrente
+
+-> Dois arquivos:
+    Agência (Cod, Nome, Gerente)
+    Conta-Corrente (Cod, CodAgencia, Saldo)
+
+-> Usuário deve poder escolher o que quer cadastrar
+-> Dados devem ser lidos do teclado
+->Aplicação deve ter opção de Cadastrar, Ler ou Sair
+
+*/
